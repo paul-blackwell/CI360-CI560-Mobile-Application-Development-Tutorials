d115eefe35e9bbe611b318835e3a2a0dfb9f7084
Paul Blackwell <blackwellpaul34@gmail.com>
2020-12-29 12:51:48 +0000

    Week 2

---
+++ Week 2/my-project/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+       <Text>Other text on screen</Text>
+      <Text style={styles.text}>Hello world</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    fontSize: 14,
+    marginBottom: '80%'
+  }
+});

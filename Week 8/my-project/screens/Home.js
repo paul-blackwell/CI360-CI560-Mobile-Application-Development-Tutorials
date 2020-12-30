dbe559a84997807c4d4cbacb2046cfe440a69a75
Paul Blackwell <blackwellpaul34@gmail.com>
2020-12-30 23:17:38 +0000

    Week 8 - (70%) complete

--- Week 8/my-project/screens/Home.js
+++ Week 8/my-project/screens/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, Text, View, FlatList} from 'react-native';
+import { Dimensions, StyleSheet, Text, View, FlatList } from 'react-native';
 
 import Data from '../data/Data';
 
@@ -9,46 +9,29 @@ import Data from '../data/Data';
 
 export default function Home() {
 
-  const renderItem = ({ item }) => (
-    <View style={styles.itemContainer}>
-      <Text>{item.id}</Text>
-    </View>
-  );
-
   return (
-    // <View style={styles.container}>
-    //   <View style={styles.box}><Text>Box 1</Text></View>
-    //   <View style={styles.box}><Text>Box 2</Text></View>
-    // </View>
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={Data}
-        numColumns={3}
-        renderItem={renderItem}
-        keyExtractor={item => item.id.toString()}
-      />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View style={styles.item}><Text>Box 1</Text></View>
+      <View style={styles.item}><Text>Box 2</Text></View>
+      <View style={styles.item}><Text>Box 3</Text></View>
+      <View style={styles.item}><Text>Box 4</Text></View>
+      <View style={styles.item}><Text>Box 5</Text></View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //flexDirection: 'row',
-    //justifyContent: 'space-around'
+    alignItems: 'center',
+    margin: 4,
   },
-  itemContainer: {
-    width: '100%',
-    backgroundColor: '#3884FF'
+  item: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '95%',
+    height: 50,
+    margin: 4,
+    backgroundColor: '#3884FF',
   }
-  // box: {
-  //   justifyContent: 'center',
-  //   alignItems: 'center',
-  //   width: 50,
-  //   height: 50,
-  //   backgroundColor: '#fff',
-  //   borderColor: '#000000',
-  //   borderWidth: 1,
-  //   margin: 4
-  // }
 });

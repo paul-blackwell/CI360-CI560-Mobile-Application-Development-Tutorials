bb72aa0431ca19169dc90f91a2b051f5d46ecc69
Paul Blackwell <blackwellpaul34@gmail.com>
2021-01-20 16:43:44 +0000

    Added icons to the Item component

--- Week 9/my-project/components/Item.js
+++ Week 9/my-project/components/Item.js
@@ -1,33 +1,35 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
 
-const Item = ({ id, name, quantity, price, increaseItemQuantity, decreaseItemQuantity, removeItem}) => {
+const Item = ({ id, name, quantity, price, increaseItemQuantity, decreaseItemQuantity, removeItem }) => {
 
     return (
         <View>
             <Text>{name}</Text>
             <Text>x{quantity}</Text>
             <Text>£{(price * quantity).toFixed(2)}</Text>
-            <Button
-                onPress={() => {increaseItemQuantity(id)}}
-                title='+'
-            />
-            <Button
-                onPress={() => {decreaseItemQuantity(id)}}
-                title='-'
-            />
-            <Button
-                onPress={() => {removeItem(id)}}
-                title='remove'
-            />
+
+            <TouchableOpacity  onPress={() => { increaseItemQuantity(id) }}>
+                <AntDesign name="plus" size={24} color="black" />
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => { decreaseItemQuantity(id) }}>
+                <AntDesign name="minus" size={24} color="black" />
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => { removeItem(id) }}>
+                <AntDesign name="close" size={24} color="black" />
+            </TouchableOpacity>
+
         </View>
     )
 };
 
 
-// const styles = StyleSheet.create({
+const styles = StyleSheet.create({
 
-// });
+});
 
 export default Item;

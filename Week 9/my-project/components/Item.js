bf2df555e486cf2fee480b014df29a7e9e41e526
Paul Blackwell <blackwellpaul34@gmail.com>
2021-01-20 14:42:12 +0000

    Week 9 - can now update items quantities

--- Week 9/my-project/components/Item.js
+++ Week 9/my-project/components/Item.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 
 
-const Item = ({ id, name, quantity, price, removeItem }) => {
+const Item = ({ id, name, quantity, price, increaseItemQuantity, decreaseItemQuantity, removeItem}) => {
 
     return (
         <View>
             <Text>{name}</Text>
             <Text>x{quantity}</Text>
             <Text>£{(price * quantity).toFixed(2)}</Text>
+            <Button
+                onPress={() => {increaseItemQuantity(id)}}
+                title='+'
+            />
+            <Button
+                onPress={() => {decreaseItemQuantity(id)}}
+                title='-'
+            />
             <Button
                 onPress={() => {removeItem(id)}}
                 title='remove'
--- Week 9/my-project/screens/Meals.js
+++ Week 9/my-project/screens/Meals.js
@@ -1,20 +1,61 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import DATA from '../data/data'
 import Item from '../components/Item';
 
 export default function Meals() {
 
+    // Initialise state to be our Data '{ id: 1, name: 'Rice', quantity: 1, price: 0.40   }...,
     const [meals, updateMeals] = useState(DATA);
 
 
+    //This will remove an item based on its ID from the state
     const handleRemoveItem = (id) => {
         updateMeals(meals.filter(meal => meal.id !== id))
     }
 
+    /**
+     * This will increase an item's quantity by one when 
+     * fired based on its ID
+     */
+    const handleIncreaseItemQuantity = (id) => {
+        const newUpdatedMeals = meals;
+        newUpdatedMeals.forEach(meal => {
+            if (meal.id === id) {
+                    meal.quantity++;
+            }
+        })
+        updateMeals([...newUpdatedMeals])
+    }
+
+    /**
+     * This will decrease an item's quantity by one when 
+     * fired based on its ID, will only do this if
+     * the item's quantity is greater than one
+     */
+    const handleDecreaseItemQuantity = (id) => {
+        const newUpdatedMeals = meals;
+        newUpdatedMeals.forEach(meal => {
+            if (meal.id === id) {
+                if(meal.quantity > 1){
+                    meal.quantity--;
+                }
+            }
+        })
+        updateMeals([...newUpdatedMeals])
+    }
+
 
     const renderItem = ({ item }) => (
-        <Item id={item.id} name={item.name} quantity={item.quantity} price={item.price} removeItem={handleRemoveItem} />
+        <Item
+            id={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            removeItem={handleRemoveItem}
+            increaseItemQuantity={handleIncreaseItemQuantity}
+            decreaseItemQuantity={handleDecreaseItemQuantity}
+        />
     );
 
 
